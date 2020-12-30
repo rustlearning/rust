1571d3665f98004086544d40b6981d6ff33b284e
rustlearning <76766774+rustlearning@users.noreply.github.com>
2020-12-30 12:59:53 +0530

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,10 @@
+//my very first rust program
+fn main(){
+    println!("Hello World! from Rust:)");
+}
+//this is a linecomment, linecomments are written in rust following "//" and they are ristricted only to a line.
+/*this is a block comment, block comments are written with a starting and ending marks,
+like this "/*" "*/" and they span outupto from opening to closing end.
+these comments can be nested inside each other.*/
+//these are 2 types of regular comments in rust other than doc comments.
+//line comments are recommended to be used.
